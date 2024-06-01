237fcf3000eef3278eb2e260e668ed3b2a6ad597
Kirushikan_Ketheeswaran <kirushikanketheeswaran@gmail.com>
2024-06-01 06:10:44 +0530

    final work add error

--- server/routes/investor.request.route.js
+++ server/routes/investor.request.route.js
@@ -5,6 +5,6 @@ const requestController = require("../controllers/investor.request.controller");
 router.post("/createInvestorRequest", requestController.createUser);
 router.get("/findInvestorRequest", requestController.findUser);
 router.delete("/deleteInvestorRequest/:id", requestController.deleteUser);
-router.post("/sendMail/:id", requestController.sendWelcomeEmail);
+router.post("/sendMail/:_id", requestController.sendWelcomeEmail);
 
 module.exports = router;
