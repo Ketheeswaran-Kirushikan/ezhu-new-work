65657a0ffad19e5b822599a2943f809920782108
Ketheeswaran-Kirushikan <kirushikanketheeswaran@gmail.com>
2025-05-17 23:59:19 +0530

    python update

--- client/src/context/Config.js
+++ client/src/context/Config.js
@@ -1,3 +1,3 @@
-const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3002';
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'ezhu-new-work-production.up.railway.app';
 
 export default backendUrl;
